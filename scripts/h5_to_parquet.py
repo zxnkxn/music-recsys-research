6c991052d8f9df9fba7eb932b8c578641c83791e
Stanislav Zinkin <st.zinkin@yandex.ru>
2025-12-08 01:52:20 +0300

    Limit h5_to_parquet.py to essential fields

--- scripts/h5_to_parquet.py
+++ scripts/h5_to_parquet.py
@@ -8,10 +8,41 @@ import pyarrow.parquet as pq
 from scripts import hdf5_getters
 
 # Collect all getter functions (except get_num_songs)
+# all_getters = {
+#     name: func
+#     for name, func in inspect.getmembers(hdf5_getters, inspect.isfunction)
+#     if name.startswith("get_") and name != "get_num_songs"
+# }
+useful_getters = [
+    "get_artist_id",
+    "get_artist_name",
+    "get_song_id",
+    "get_title",
+    "get_duration",
+    "get_year",
+    "get_release",
+    "get_artist_familiarity",
+    "get_artist_hotttnesss",
+    "get_song_hotttnesss",
+    "get_artist_terms",
+    "get_artist_terms_freq",
+    "get_artist_terms_weight",
+    "get_artist_location",
+    "get_artist_latitude",
+    "get_artist_longitude",
+    "get_tempo",
+    "get_similar_artists",
+    "get_danceability",
+    "get_energy",
+    "get_loudness",
+    "get_key",
+    "get_mode",
+]
+
 all_getters = {
     name: func
     for name, func in inspect.getmembers(hdf5_getters, inspect.isfunction)
-    if name.startswith("get_") and name != "get_num_songs"
+    if name in useful_getters
 }
 
 
